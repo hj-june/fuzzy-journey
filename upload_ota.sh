806671f118090db91c2f4eacedd6e2c0eb87a34e
Hyun Joon Kim <hj@junelife.com>
2021-10-06 16:56:01 -0400

    finish baseline work before testing with AWS

--- upload_ota.sh
+++ upload_ota.sh
@@ -21,17 +21,37 @@ full_upload_path=""
 aws_creds=""
 
 download_build() {
-  aws s3 cp ${full_download_path} . && echo "Downloading ${full_download_path}" || echo "FAILED TO DOWNLOAD: ${full_download_path}"
+  aws s3 cp ${full_download_path} . && echo "Download successful: ${full_download_path}" || { echo "Download FAILED: ${full_download_path}"; exit 1; }
 }
 
 upload_ota() {
-  aws s3 cp ${build_zipfile} ${full_upload_path} && echo "Uploading ${build_zipfile} to OTA Server!" || echo "FAILED TO UPLOAD ${build_zipfile}"
+  #aws s3 cp ${build_zipfile} ${full_upload_path} && echo "Upload successful: ${build_zipfile}" || { echo "Upload FAILED ${build_zipfile}"; exit 1; }
+  pass
 }
 
+do_magic() {
+  unzip -q ${build_zipfile} -d ${build_name} && echo "Unzip successful: ${build_zipfile} "|| { echo "FAILED to unzip: ${build_zipfile}"; exit 1; }
+  sync
+
+  origin_jota="app.enc.jota"
+  changed_jota1="${device}-${build}"
+  changed_jota2="${device}-${build}0000"
+
+  origin_jotc=$(find ./${build_name} -name "*systemPlus-ABRFP.jotc" -exec basename {} \;)
+  changed_jotc1="${device}-${build}${build}.jotc"
+
+  cp -v "./${build_name}/${origin_jota}" "./${build_name}/${changed_jota1}" || { echo "FAILED to make copy of ${origin_jota}"; exit 1; }
+  cp -v "./${build_name}/${origin_jota}" "./${build_name}/${changed_jota2}" || { echo "FAILED to make copy of ${origin_jota}"; exit 1; }
+  cp -v "./${build_name}/${origin_jotc}" "./${build_name}/${changed_jotc1}" || { echo "FAILED to make copy of ${origin_jotc}"; exit 1; }
+  sync
+}
+
+# debug print
 echo "device is : $device"
 echo "build is : $build"
 echo "full build name is: ${build_name}"
 echo "full zip file is: ${build_zipfile}"
 
-download_build
-upload_ota
+#download_build
+#upload_ota
+do_magic
